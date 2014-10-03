c6e0c07b19e512e85ed0e6066d9d1a95987b77fe
Avner Goncalves <avner.goncalves@s2it.com.br>
2014-10-03 14:50:33 -0300

    Add utf8 configuration

--- networkapi/environment_settings.py
+++ networkapi/environment_settings.py
@@ -25,7 +25,7 @@ from networkapi.log import Log
 LOG_FILE = '/tmp/networkapi.log'
 LOG_LEVEL = logging.DEBUG
 LOG_DAYS = 10
-LOG_SHOW_SQL = True
+LOG_SHOW_SQL = False
 LOG_USE_STDOUT = True
 
 VLAN_CACHE_TIME = None
@@ -40,10 +40,10 @@ Log.init_log(LOG_FILE, LOG_DAYS, LOG_LEVEL, use_stdout=LOG_USE_STDOUT)
 
 # Configurações de banco de dados
 # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_ENGINE = 'django.db.backends.mysql'
-DATABASE_NAME = 'telecom_test'  # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'  # Not used with sqlite3.
-DATABASE_PASSWORD = ''  # Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'telecom'      # Or path to database file if using sqlite3.
+DATABASE_USER = 'root'         # Not used with sqlite3.
+DATABASE_PASSWORD = 'root'         # Not used with sqlite3.
 # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_HOST = 'localhost'
 # Set to empty string for default. Not used with sqlite3.
@@ -71,3 +71,7 @@ CACHE_BACKEND = 'memcached://localhost:11211/'
 
 # Diretório dos arquivos dos scripts
 SCRIPTS_DIR = os.path.abspath(os.path.join(__file__, '../../scripts'))
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
