e301f53db17b275c61bfef8fc8752f2c1521f4e4
William Vedroni <william.silva@s2it.com.br>
2014-10-10 14:35:24 -0300

    Add new url

--- networkapi/pools/urls.py
+++ networkapi/pools/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'networkapi.pools.views',
     url(r'^pools/$', 'pool_list'),
-    url(r'^pools/delete/$', 'delete')
+    url(r'^pools/insert/$', 'pool_insert'),
+    url(r'^pools/list_healthchecks/$', 'healthcheck_list'),
+    url(r'^pools/delete/$', 'delete'),
+    url(r'^pools/remove/$', 'remove'),
 )
