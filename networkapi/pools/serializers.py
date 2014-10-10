7ec8639fea8cd0a5e1631a76c26721e8d44187c6
William Vedroni <william.silva@s2it.com.br>
2014-10-10 14:35:14 -0300

    Add new fields serializer Pool

--- networkapi/pools/serializers.py
+++ networkapi/pools/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from networkapi.requisicaovips.models import ServerPool
+from networkapi.healthcheckexpect.models import Healthcheck
 
 
 class ServerPoolSerializer(serializers.ModelSerializer):
+
+    environment = serializers.RelatedField(
+        source='environment.name'
+    )
+
     class Meta:
         model = ServerPool
-        fields = ('id', 'identifier', 'default_port', 'healthcheck')
+        fields = (
+            'id',
+            'identifier',
+            'default_port',
+            'healthcheck',
+            'environment',
+            'pool_created'
+        )
+
+
+class HealthcheckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Healthcheck
+        fields = ('id', 'identifier', 'healthcheck_type', 'healthcheck_request', 'healthcheck_expect', 'destination')
