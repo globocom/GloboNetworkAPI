26c753dcc56b3897922631c96310fab70264916b
laurapanzariello <laura.panzariello@Mac179028-Laura.local>
2015-08-11 18:49:09 -0300

    Edit interface_do_ambiente

--- networkapi/interface/resource/InterfaceEnvironmentResource.py
+++ networkapi/interface/resource/InterfaceEnvironmentResource.py
@@ -83,3 +83,48 @@ class InterfaceEnvironmentResource(RestResource):
             return self.response_error(3, x)
         except InterfaceError:
            return self.response_error(1)
+
+    def handle_delete(self, request, user, *args, **kwargs):
+        """Treat requests POST to add Rack.
+
+        URL: interface/associar-ambiente/
+        """
+        try:
+            self.log.info("Edita os ambientes associados a uma interface")
+
+            # User permission
+            if not has_perm(user, AdminPermission.EQUIPMENT_MANAGEMENT, AdminPermission.WRITE_OPERATION):
+                self.log.error(
+                    u'User does not have permission to perform the operation.')
+                raise UserNotAuthorizedError(None)
+
+            # Load XML data
+            xml_map, attrs_map = loads(request.raw_post_data)
+
+            # XML data format
+            networkapi_map = xml_map.get('networkapi')
+            if networkapi_map is None:
+                return self.response_error(3, u'There is no value to the networkapi tag  of XML request.')
+
+            interface_map = networkapi_map.get('interface')
+            if interface_map is None:
+                return self.response_error(3, u'There is no value to the interface tag  of XML request.')
+
+            # Get XML data
+            interface = interface_map.get('interface')
+            interface = int(interface)
+
+            #deletar associacoes
+            interface_list = EnvironmentInterface.objects.all().filter(interface__exact=interface)
+            for var in interface_list:
+                var.delete(user)
+
+            return self.response(dumps_networkapi({}))
+
+        except InvalidValueError, e:
+            return self.response_error(269, e.param, e.value)
+        except XMLError, x:
+            self.log.error(u'Erro ao ler o XML da requisição.')
+            return self.response_error(3, x)
+        except InterfaceError:
+           return self.response_error(1)
