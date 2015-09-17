16641622bc964d25e20a159d8f4b7f138e21812e
victor-mendes-eduardo <victor.mendes.eduardo@gmail.com>
2015-09-17 11:42:33 -0300

    Refactor '/grupovirtual/*', '/aperms/*', '/direitosgrupoequipamento/*', '/divisiondc/*', '/grupol3/*', '/ipconfig/*', '/logicalenvironment/*' and '/perms/*' urls

---
+++ networkapi/ambiente/urls_ipconfig.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from django.conf.urls import patterns, url
+from networkapi.ambiente.resource.EnvironmentIpConfigResource import EnvironmentIpConfigResource
+
+env_ip_conf_resource = EnvironmentIpConfigResource()
+
+urlpatterns = patterns('',
+  url(r'^$', env_ip_conf_resource.handle_request,
+       name="ipconfig.associate"),
+)
--- networkapi/urls.py
+++ networkapi/urls.py
@@ -18,29 +18,8 @@
 from django.conf.urls.defaults import *
 from django.http import HttpResponse
 
-from networkapi.ambiente.resource.DivisionDcAddResource import DivisionDcAddResource
-from networkapi.ambiente.resource.DivisionDcAlterRemoveResource import DivisionDcAlterRemoveResource
-from networkapi.ambiente.resource.DivisionDcGetAllResource import DivisionDcGetAllResource
-from networkapi.ambiente.resource.GroupL3AddResource import GroupL3AddResource
-from networkapi.ambiente.resource.GroupL3AlterRemoveResource import GroupL3AlterRemoveResource
-from networkapi.ambiente.resource.GroupL3GetAllResource import GroupL3GetAllResource
-from networkapi.ambiente.resource.LogicalEnvironmentAddResource import LogicalEnvironmentAddResource
-from networkapi.ambiente.resource.LogicalEnvironmentAlterRemoveResource import LogicalEnvironmentAlterRemoveResource
-from networkapi.ambiente.resource.LogicalEnvironmentGetAllResource import LogicalEnvironmentGetAllResource
-from networkapi.ambiente.resource.EnvironmentIpConfigResource import EnvironmentIpConfigResource
-
-from networkapi.grupo.resource.GrupoResource import DireitoGrupoEquipamentoResource
-from networkapi.grupo.resource.AdministrativePermissionByGroupUserResource import AdministrativePermissionByGroupUserResource
-from networkapi.grupo.resource.AdministrativePermissionAddResource import AdministrativePermissionAddResource
-from networkapi.grupo.resource.AdministrativePermissionAlterRemoveResource import AdministrativePermissionAlterRemoveResource
-from networkapi.grupo.resource.AdministrativePermissionGetAllResource import AdministrativePermissionGetAllResource
-from networkapi.grupo.resource.AdministrativePermissionGetByIdResource import AdministrativePermissionGetByIdResource
-from networkapi.grupo.resource.PermissionGetAllResource import PermissionGetAllResource
-
 from networkapi.interface.resource.InterfaceTypeGetAllResource import InterfaceTypeGetAllResource
 from networkapi.interface.resource.InterfaceChannelResource import InterfaceChannelResource
-
-from networkapi.grupovirtual.resource.GrupoVirtualResource import GroupVirtualResource
 
 from networkapi.eventlog.resource.EventLogFindResource import EventLogFindResource
 from networkapi.eventlog.resource.EventLogChoiceResource import EventLogChoiceResource
@@ -72,36 +51,10 @@ list_all_racks_resource = RackListAllResource()
 list_rack_environment_resource = RackEnvironmentResource()
 get_rack_by_equip_resource = RackGetByEquipResource()
 
-env_ip_conf_resource = EnvironmentIpConfigResource()
-
-division_dc_add_resource = DivisionDcAddResource()
-division_dc_alter_remove_resource = DivisionDcAlterRemoveResource()
-division_dc_get_all_resource = DivisionDcGetAllResource()
-
-group_l3_add_resource = GroupL3AddResource()
-group_l3_alter_remove_resource = GroupL3AlterRemoveResource()
-group_l3_get_all_resource = GroupL3GetAllResource()
-
-logical_environment_add_resource = LogicalEnvironmentAddResource()
-logical_environment_alter_remove_resource = LogicalEnvironmentAlterRemoveResource()
-logical_environment_get_all_resource = LogicalEnvironmentGetAllResource()
-
-virtual_group_resource = GroupVirtualResource()
-
 interface_type_get_all_resource = InterfaceTypeGetAllResource()
 
 interface_channel_resource = InterfaceChannelResource()
 
-aperms_get_by_group = AdministrativePermissionByGroupUserResource()
-aperms_add_resource = AdministrativePermissionAddResource()
-aperms_get_by_pk_resource = AdministrativePermissionGetByIdResource()
-aperms_get_all_resource = AdministrativePermissionGetAllResource()
-aperms_alter_remove_resource = AdministrativePermissionAlterRemoveResource()
-perms_get_all_resource = PermissionGetAllResource()
-
-access_right_resource = DireitoGrupoEquipamentoResource()
-
-# eventlog
 eventlog_find_resource = EventLogFindResource()
 eventlog_choice_resource = EventLogChoiceResource()
 
@@ -119,6 +72,9 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('',
+   #networkapi app healthcheck
+   url(r'^check$', check_action.check, name='check'),
+
    #equipamento
    url(r'^equipamento/', include('networkapi.equipamento.urls')),
    url(r'^equipment/', include('networkapi.equipamento.urls_equipment')),
@@ -134,33 +90,13 @@ urlpatterns += patterns('',
    #ambiente
    url(r'^ambiente/', include('networkapi.ambiente.urls')),
    url(r'^environment/', include('networkapi.ambiente.urls_environment')),
+   url(r'^divisiondc/', include('networkapi.ambiente.urls_divisiondc')),
+   url(r'^groupl3/', include('networkapi.ambiente.urls_groupl3')),
+   url(r'^logicalenvironment/', include('networkapi.ambiente.urls_logicalenvironment')),
+   url(r'^ipconfig/', include('networkapi.ambiente.urls_ipconfig')),
 
    #rules
    url(r'^rule/', include('networkapi.blockrules.urls')),
-
-   url(r'^ipconfig/$', env_ip_conf_resource.handle_request,
-       name="ipconfig.associate"),
-
-   url(r'^divisiondc/$', division_dc_add_resource.handle_request,
-       name='division_dc.add'),
-   url(r'^divisiondc/all/$', division_dc_get_all_resource.handle_request,
-       name='division_dc.get.all'),
-   url(r'^divisiondc/(?P<id_divisiondc>[^/]+)/$',
-       division_dc_alter_remove_resource.handle_request, name='division_dc.update.remove.by.pk'),
-
-   url(r'^groupl3/$', group_l3_add_resource.handle_request,
-       name='group_l3.add'),
-   url(r'^groupl3/all/$', group_l3_get_all_resource.handle_request,
-       name='group_l3.get.all'),
-   url(r'^groupl3/(?P<id_groupl3>[^/]+)/$',
-       group_l3_alter_remove_resource.handle_request, name='group_l3.update.remove.by.pk'),
-
-   url(r'^logicalenvironment/$', logical_environment_add_resource.handle_request,
-       name='logical_environment.add'),
-   url(r'^logicalenvironment/all/$', logical_environment_get_all_resource.handle_request,
-       name='logical_environment.get.all'),
-   url(r'^logicalenvironment/(?P<id_logicalenvironment>[^/]+)/$',
-       logical_environment_alter_remove_resource.handle_request, name='logical_environment.update.remove.by.pk'),
 
    #vlan
    url(r'^vlan/', include('networkapi.vlan.urls')),
@@ -188,14 +124,14 @@ urlpatterns += patterns('',
    url(r'^environmentvip/', include('networkapi.requisicaovips.urls_environmentvip')),
    url(r'^optionvip/', include('networkapi.requisicaovips.urls_optionvip')),
 
+   #grupovirtual
+   url(r'^grupovirtual/', include('networkapi.grupovirtual.urls')),
+
    #interface
    url(r'^interface/', include('networkapi.interface.urls')),
    url(r'^int/', include('networkapi.interface.urls_int')),
 
-   url(r'^grupovirtual/$', virtual_group_resource.handle_request,
-       name='virtual_group.add.remove'),
-
-   #usario
+   #usuario
    url(r'^usuario/', include('networkapi.usuario.urls')),
    url(r'^user/', include('networkapi.usuario.urls_user')),
    url(r'^authenticate/', include('networkapi.usuario.urls_authenticate')),
@@ -209,30 +145,9 @@ urlpatterns += patterns('',
    url(r'^ugroup/', include('networkapi.grupo.urls')),
    url(r'^egroup/', include('networkapi.grupo.urls_egroup')),
    url(r'^egrupo/', include('networkapi.grupo.urls_egrupo')),
-
-   url(r'^perms/all/$', perms_get_all_resource.handle_request,
-       name='permission.get.all'),
-   url(r'^aperms/$', aperms_add_resource.handle_request,
-       name='administrative.permission.add'),
-   url(r'^aperms/all/$', aperms_get_all_resource.handle_request,
-       name='administrative.permission.get.all'),
-   url(r'^aperms/(?P<id_perm>[^/]+)/$', aperms_alter_remove_resource.handle_request,
-       name='administrative.permission.update.remove.by.pk'),
-   url(r'^aperms/group/(?P<id_ugroup>[^/]+)/$', aperms_get_by_group.handle_request,
-       name='administrative.permission.get.by.group'),
-   url(r'^aperms/get/(?P<id_perm>[^/]+)/$', aperms_get_by_pk_resource.handle_request,
-       name='administrative.permission.get.by.pk'),
-
-   url(r'^direitosgrupoequipamento/$', access_right_resource.handle_request,
-       name='access_right.search.insert'),
-   url(r'^direitosgrupoequipamento/ugrupo/(?P<id_grupo_usuario>[^/]+)/$',
-       access_right_resource.handle_request, name='access_right.search.by.ugroup'),
-   url(r'^direitosgrupoequipamento/egrupo/(?P<id_grupo_equipamento>[^/]+)/$',
-       access_right_resource.handle_request, name='access_right.search.by.egroup'),
-   url(r'^direitosgrupoequipamento/(?P<id_direito>[^/]+)/$',
-       access_right_resource.handle_request, name='access_right.search.update.remove.by.pk'),
-
-   url(r'^check$', check_action.check, name='check'),
+   url(r'^perms/', include('networkapi.grupo.urls_perms')),
+   url(r'^aperms/', include('networkapi.grupo.urls_aperms')),
+   url(r'^direitosgrupoequipamento/', include('networkapi.grupo.urls_direitosgrupoequipamento')),
 
    #filter
    url(r'^filter/', include('networkapi.filter.urls')),
