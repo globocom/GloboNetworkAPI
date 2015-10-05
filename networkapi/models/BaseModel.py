d5fd56c43a805c34d0f64c03eecf83a4bd4cbc8c
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-10-05 17:33:38 -0300

    creating unicode method

--- networkapi/models/BaseModel.py
+++ networkapi/models/BaseModel.py
@@ -14,8 +14,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
 from django.db import models, transaction, router
 from networkapi.models.BaseManager import BaseManager
 from django.db.models.deletion import Collector
@@ -34,21 +32,32 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
+    def __unicode__(self):
+        if hasattr(self, 'nome'):
+            return u"{0}".format(self.nome)
+        elif hasattr(self, 'id'):
+            return u"{0}".format(self.id)
+        else:
+            return u"{0}".format(self.__str__())
+
     def set_authenticated_user(self, user):
         self.authenticated_user = user
 
-    def save(self, user=None, force_insert=False, force_update=False, commit=False):
+    def save(self, user=None, force_insert=False, force_update=False, commit=False, **kwargs):
         if user:
             self.set_authenticated_user(user)
-        super(BaseModel, self).save(force_insert, force_update)
+        super(BaseModel, self).save(force_insert, force_update, **kwargs)
         if commit == True:
             transaction.commit()
 
-    def delete(self, user=None):
+    def delete(self, **kwargs):
         '''
         Replace  super(BaseModel, self).delete()
         Cause: When delete relationship in cascade  default no have attribute User to Log.
         '''
+
+        user = kwargs.get('user', None)
+
         using = router.db_for_write(self.__class__, instance=self)
         assert self._get_pk_val() is not None, "%s object can't be deleted because its %s attribute is set to None." % (
             self._meta.object_name, self._meta.pk.attname)
