d78be02e16cffdf545b4e576673e0e10fffe08e0
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-10-21 19:01:31 -0200

    improving display

--- networkapi/requisicaovips/admin/requisicao_vip.py
+++ networkapi/requisicaovips/admin/requisicao_vip.py
@@ -3,7 +3,18 @@ from django.contrib import admin
 
 class RequisicaoVipsAdmin(admin.ModelAdmin):
     search_fields = ["id", "ip"]
-    list_display = ["id", "validado", "vip_criado", "ip", "healthcheck_expect"]
+    list_display = ["id", "validado", "vip_criado", "get_ip", "get_healthcheck"]
     list_filter = ('validado','vip_criado')
 
 
+    def get_ip(self, obj):
+        return u"{} (id {})".format(obj.ip.ip_formated, obj.ip.id)
+
+    get_ip.short_description = 'IP'
+    #get_ip.admin_order_field = 'book__author'
+
+    def get_healthcheck(self, obj):
+        return obj.healthcheck_expect.expect_string
+
+    get_healthcheck.short_description = 'Healthcheck expect'
+    get_healthcheck.admin_order_field = 'healthcheck_expect__expect_string'
--- networkapi/requisicaovips/admin/server_pool.py
+++ networkapi/requisicaovips/admin/server_pool.py
@@ -2,8 +2,12 @@
 from django.contrib import admin
 
 class ServerPoolAdmin(admin.ModelAdmin):
-    search_fields = ["identifier", "ip"]
-    list_display = ["id", "identifier", "healthcheck", "default_port", "pool_created"]
+    search_fields = ["identifier", "id"]
+    list_display = ["id", "identifier", "get_healthcheck", "default_port", "pool_created"]
     list_filter = ('pool_created',)
 
+    def get_healthcheck(self, obj):
+        return obj.healthcheck.healthcheck_type
 
+    get_healthcheck.short_description = 'Healthcheck'
+    get_healthcheck.admin_order_field = 'healthcheck__healthcheck_type'
