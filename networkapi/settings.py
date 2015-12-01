f4bead4f3f0c17071d6b3bd6b50d1a34f879fede
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-12-01 17:23:55 -0200

    bug fix: broker environment variables

--- networkapi/settings.py
+++ networkapi/settings.py
@@ -428,12 +428,8 @@ VIP_REALS_v6_CHECK = 'gerador_vips -i %s --id_ipv6 %s --port_ip %s --port_vip %s
 #       QUEUE SETTINGS
 ##################################
 
-NETWORKAPI_BROKER_CONNECT_TIMEOUT=os.getenv('NETWORKAPI_BROKER_CONNECT_TIMEOUT','2')
-NETWORKAPI_BROKER_URI=os.getenv('NETWORKAPI_BROKER_URI',u"failover:(tcp://localhost:61613,tcp://server2:61613,tcp://server3:61613)?randomize=falsa,startupMaxReconnectAttempts=2,maxReconnectAttempts=1e")
-
-QUEUE_DESTINATION = u"/topic/networkapi_queue"
-QUEUE_BROKER_URI = NETWORKAPI_BROKER_URI
-QUEUE_BROKER_CONNECT_TIMEOUT = int(NETWORKAPI_BROKER_CONNECT_TIMEOUT)
+BROKER_CONNECT_TIMEOUT=os.getenv('NETWORKAPI_BROKER_CONNECT_TIMEOUT','2')
+BROKER_URI=os.getenv('NETWORKAPI_BROKER_URI',u"failover:(tcp://localhost:61613,tcp://server2:61613,tcp://server3:61613)?randomize=falsa,startupMaxReconnectAttempts=2,maxReconnectAttempts=1e")
 
 ###################################
 #    PATH ACLS
