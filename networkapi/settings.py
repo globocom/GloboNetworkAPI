664eb8cc7695e003f0de1b0e178af143dbdae4d6
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-12-01 17:33:20 -0200

    BROKER_DESTINATION was not reading the value from an environment variable

--- networkapi/settings.py
+++ networkapi/settings.py
@@ -428,9 +428,10 @@ VIP_REALS_v6_CHECK = 'gerador_vips -i %s --id_ipv6 %s --port_ip %s --port_vip %s
 #       QUEUE SETTINGS
 ##################################
 
-BROKER_CONNECT_TIMEOUT=os.getenv('NETWORKAPI_BROKER_CONNECT_TIMEOUT','2')
+BROKER_CONNECT_TIMEOUT=os.getenv('NETWORKAPI_BROKER_CONNECT_TIMEOUT', '2')
+BROKER_DESTINATION=os.getenv('NETWORKAPI_BROKER_DESTINATION', "/topic/networkapi_queue")
 BROKER_URI=os.getenv('NETWORKAPI_BROKER_URI',u"failover:(tcp://localhost:61613,tcp://server2:61613,tcp://server3:61613)?randomize=falsa,startupMaxReconnectAttempts=2,maxReconnectAttempts=1e")
-BROKER_DESTINATION="/topic/networkapi_queue"
+
 
 ###################################
 #    PATH ACLS
