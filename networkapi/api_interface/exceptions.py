51cecd3fa789985c3c5fc5e0308af5f4db738032
Marcus Vinicius G. Cesario <marcusgc@gmail.com>
2015-12-23 11:54:40 -0200

    Add error for interface trunk configuration when there is no vlan range for dot1q config

--- networkapi/api_interface/exceptions.py
+++ networkapi/api_interface/exceptions.py
@@ -37,6 +37,10 @@ class InterfaceTemplateException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'No template or multiple templates found for interface configuration.'
 
+class InterfaceTrunkAllowedVlanException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'There is no vlan range specified to configure interface in dot1q mode.'
+
 class InterfaceException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'Error: .'
