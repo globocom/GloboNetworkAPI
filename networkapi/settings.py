8cd21571569ccfe299bb75c0087d812504b386e8
Marcus Vinicius G. Cesario <marcusgc@gmail.com>
2016-03-31 20:05:19 -0300

    debug flag

--- networkapi/settings.py
+++ networkapi/settings.py
@@ -38,7 +38,6 @@ NETWORKAPI_USE_NEWRELIC = os.getenv('NETWORKAPI_USE_NEWRELIC', '0') == 1
 
 # Aplicação rodando em modo Debug
 DEBUG = os.getenv('NETWORKAPI_DEBUG', '0') == '1'
-DEBUG = 0
 CI = os.getenv('CI', '0') == '1'
 
 NETWORKAPI_LOG_FILE = os.getenv('NETWORKAPI_LOG_FILE', '/tmp/networkapi.log')
