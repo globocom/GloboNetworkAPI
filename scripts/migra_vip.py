dc35c9a2ee4617ed49910bcb8f7aa846b831db3c
edersonbrilhante <ederson.brilhante@corp.globo.com>
2016-06-17 09:08:37 -0300

    fix script sync versions of vips

--- scripts/migra_vip.py
+++ scripts/migra_vip.py
@@ -1,17 +1,16 @@
 from django.db.transaction import commit_on_success
 
-from networkapi.api_vip_request.models import RequisicaoVips, VipPortToPool
 from networkapi.api_vip_request.syncs import old_to_new
+from networkapi.requisicaovips.models import RequisicaoVips
 
 
 @commit_on_success
-def run():
+def sync_vip():
     # migrate old vips
     vip_requests = RequisicaoVips.objects.all()
     for vip_request in vip_requests:
-        pools = VipPortToPool.get_by_vip_id(vip_request.id)
-        old_to_new(vip_request, pools)
+        old_to_new(vip_request)
 
-    # vip_requests = VipRequest.objects.all()
-    # for vip_request in vip_requests:
-    #     new_to_old(vip_request)
+
+def run():
+    sync_vip()
