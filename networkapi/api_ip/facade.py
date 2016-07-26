20c2e1ccf8b18939bff60be2538771cb4265a063
ederson.brilhante <ederson.brilhante@corp.globo.com>
2016-07-26 20:02:02 -0300

    fix delete ip when delete vip

--- networkapi/api_ip/facade.py
+++ networkapi/api_ip/facade.py
@@ -1,18 +1,19 @@
 # -*- coding:utf-8 -*-
 import logging
 
-from networkapi.ip.models import Ip, Ipv6
+from networkapi.ip.models import Ip
+from networkapi.ip.models import Ipv6
 
 log = logging.getLogger(__name__)
 
 
 def delete_ipv4_list(ipv4_list):
     for ipv4 in ipv4_list:
-        ip = Ip.object.get(id=ipv4)
+        ip = Ip.objects.get(id=ipv4)
         ip.delete()
 
 
 def delete_ipv6_list(ipv6_list):
     for ipv6 in ipv6_list:
-        ip = Ipv6.object.get(id=ipv6)
+        ip = Ipv6.objects.get(id=ipv6)
         ip.delete()
