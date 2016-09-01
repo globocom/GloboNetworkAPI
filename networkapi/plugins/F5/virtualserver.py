b2703a28439b1a9b645c5ddb2bed3b5b450c3e07
edersonbrilhante <ederson.brilhante@corp.globo.com>
2016-08-31 21:49:48 -0300

    fix delete l7_rule in version >11.2 if F5 LB

--- networkapi/plugins/F5/virtualserver.py
+++ networkapi/plugins/F5/virtualserver.py
@@ -22,6 +22,7 @@ class VirtualServer(F5Base):
 
     @logger
     def delete(self, **kwargs):
+        self.__remove_all_rule(kwargs['vps_names'])
         self._lb._channel.LocalLB.VirtualServer.delete_virtual_server(
             virtual_servers=kwargs['vps_names']
         )
@@ -483,16 +484,9 @@ class VirtualServer(F5Base):
 
     @logger
     def __remove_all_rule(self, virtual_servers):
-        version_split = self._lb._version[8:len(self._lb._version)].split('.')
-        # old version
-        if version_split[0] == '11' and int(version_split[1]) <= 2:
-            self._lb._channel.LocalLB.VirtualServer.remove_all_rules(
-                virtual_servers=virtual_servers
-            )
-        else:
-            self._lb._channel.LocalLB.VirtualServer.remove_all_related_rules(
-                virtual_servers=virtual_servers
-            )
+        self._lb._channel.LocalLB.VirtualServer.remove_all_rules(
+            virtual_servers=virtual_servers
+        )
 
     @logger
     def __get_profile(self, virtual_servers):
