14c942fcc3cecfeb83f12c19b5c86a64b5d963db
juanaugusto <juan.paula@corp.globo.com>
2017-01-04 17:00:15 -0200

    When environment vip is set in network v4 or v6 create, this info were not being persisted on database

--- networkapi/ip/models.py
+++ networkapi/ip/models.py
@@ -677,7 +677,7 @@ class NetworkIPv4(BaseModel):
             # Environment vip
             if networkv4.get('environmentvip'):
                 environmentvip_model = get_model('ambiente', 'EnvironmentVip')
-                self.environmentvip = environmentvip_model().get_by_pk(
+                self.ambient_vip = environmentvip_model().get_by_pk(
                     networkv4.get('environmentvip'))
 
             # Get environments related
@@ -2820,7 +2820,7 @@ class NetworkIPv6(BaseModel):
             # has environmentvip
             if networkv6.get('environmentvip'):
                 environmentvip_model = get_model('ambiente', 'EnvironmentVip')
-                self.environmentvip = environmentvip_model().get_by_pk(
+                self.ambient_vip = environmentvip_model().get_by_pk(
                     networkv6.get('environmentvip'))
 
             # Get environments related
