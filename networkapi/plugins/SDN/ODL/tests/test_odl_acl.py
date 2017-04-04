e9e48666f4aa76b1023b9fe0fea7e6a16355ad03
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-04-04 17:08:40 -0300

    Created tests to TCP and UDP source and destination ports

--- networkapi/plugins/SDN/ODL/tests/test_odl_acl.py
+++ networkapi/plugins/SDN/ODL/tests/test_odl_acl.py
@@ -176,3 +176,123 @@ class OpenDayLightACLTestCase(NetworkApiTestCase):
         acl.build()
         assert_equal(acl.flows["flow"][0]["match"]["tcp-source-port"],
                 data["rules"][0]["l4-options"]["src-port-start"])
+
+    def test_acl_should_have_tcp_as_ip_protocol(self):
+        """ Should have tcp as ip-protocol field """
+        data = {
+            "kind": "default#acl",
+            "rules": [{
+                "id": 1,
+                "source": "10.0.0.1/32",
+                "destination": "10.0.0.2/32",
+                "protocol": "tcp"
+            }]
+        }
+        acl = AclFlowBuilder(data)
+        acl.build()
+        assert_equal(6,
+            acl.flows["flow"][0]["match"]["ip-match"]["ip-protocol"])
+
+    def test_acl_should_have_udp_as_ip_protocol(self):
+        """ Should have upd as ip-protocol field """
+        data = {
+            "kind": "default#acl",
+            "rules": [{
+                "id": 1,
+                "source": "10.0.0.1/32",
+                "destination": "10.0.0.2/32",
+                "protocol": "udp"
+            }]
+        }
+        acl = AclFlowBuilder(data)
+        acl.build()
+        assert_equal(17,
+            acl.flows["flow"][0]["match"]["ip-match"]["ip-protocol"])
+
+    def test_acl_should_have_udp_source_port(self):
+        """ Should have udp source port """
+        data = {
+            "kind": "default#acl",
+            "rules": [{
+                "id": 1,
+                "protocol": "udp",
+                "source": "10.0.0.1/32",
+                "destination": "10.0.0.2/32",
+                "l4-options": {
+                    "src-port-op": "eq",
+                    "src-port-start": "26379"
+                }
+            }]
+        }
+        acl = AclFlowBuilder(data)
+        acl.build()
+        assert_equal(acl.flows["flow"][0]["match"]["udp-source-port"],
+                data["rules"][0]["l4-options"]["src-port-start"])
+
+    def test_acl_should_have_udp_destination_port(self):
+        """ Should have udp destination port """
+        data = {
+            "kind": "default#acl",
+            "rules": [{
+                "id": 1,
+                "protocol": "udp",
+                "source": "10.0.0.1/32",
+                "destination": "10.0.0.2/32",
+                "l4-options": {
+                    "dest-port-op": "eq",
+                    "dest-port-start": "26379"
+                }
+            }]
+        }
+        acl = AclFlowBuilder(data)
+        acl.build()
+        assert_equal(acl.flows["flow"][0]["match"]["udp-destination-port"],
+                data["rules"][0]["l4-options"]["dest-port-start"])
+
+    def test_acl_should_have_tcp_source_and_destination_ports(self):
+        """ Should have tcp source and destination ports """
+        data = {
+            "kind": "default#acl",
+            "rules": [{
+                "id": 1,
+                "protocol": "tcp",
+                "source": "10.0.0.1/32",
+                "destination": "10.0.0.2/32",
+                "l4-options": {
+                    "dest-port-op": "eq",
+                    "dest-port-start": "26379",
+                    "src-port-op": "eq",
+                    "src-port-start": "26379"
+                }
+            }]
+        }
+        acl = AclFlowBuilder(data)
+        acl.build()
+        assert_equal(acl.flows["flow"][0]["match"]["tcp-source-port"],
+                data["rules"][0]["l4-options"]["src-port-start"])
+        assert_equal(acl.flows["flow"][0]["match"]["tcp-destination-port"],
+                data["rules"][0]["l4-options"]["dest-port-start"])
+
+    def test_acl_should_have_udp_source_and_destination_ports(self):
+        """ Should have udp source and destination ports """
+        data = {
+            "kind": "default#acl",
+            "rules": [{
+                "id": 1,
+                "protocol": "udp",
+                "source": "10.0.0.1/32",
+                "destination": "10.0.0.2/32",
+                "l4-options": {
+                    "dest-port-op": "eq",
+                    "dest-port-start": "26379",
+                    "src-port-op": "eq",
+                    "src-port-start": "26379"
+                }
+            }]
+        }
+        acl = AclFlowBuilder(data)
+        acl.build()
+        assert_equal(acl.flows["flow"][0]["match"]["udp-source-port"],
+                data["rules"][0]["l4-options"]["src-port-start"])
+        assert_equal(acl.flows["flow"][0]["match"]["udp-destination-port"],
+                data["rules"][0]["l4-options"]["dest-port-start"])
