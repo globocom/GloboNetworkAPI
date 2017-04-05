462dcbd0fcce68b21f7938b473515260d2414a31
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-04-05 17:32:35 -0300

    Added sleep that waits for other containers to start

--- docker-start.sh
+++ docker-start.sh
@@ -1,9 +1,10 @@
+# Waits for other containers availability
+sleep 5
 
 mysql -uroot -h netapi_db -e 'drop database if exists networkapi;'
 mysql -uroot -h netapi_db -e 'create database networkapi;'
 cd /netapi/dbmigrate; db-migrate --show-sql
 mysql -u root -h netapi_db networkapi < /netapi/dev/load_example_environment.sql
-
 
 echo -e "PYTHONPATH=\"/netapi/networkapi:/netapi/$PYTHONPATH\"" >> /etc/environment
 echo -e '#!/bin/bash\n/usr/local/bin/gunicorn -c /netapi/gunicorn.conf.py networkapi_wsgi:application' > /etc/init.d/gunicorn_networkapi
@@ -11,4 +12,5 @@ chmod 777 /etc/init.d/gunicorn_networkapi
 update-rc.d gunicorn_networkapi defaults
 
 export PYTHONPATH="/netapi/networkapi:/netapi/$PYTHONPATH"
+# cd /netapi; python manage.py runserver 0.0.0.0:8000
 /usr/local/bin/gunicorn  -c /netapi/gunicorn.conf.py networkapi_wsgi:application
