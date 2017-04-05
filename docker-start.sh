770056b23b2886afe469b644a469ebed809199af
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-04-05 19:59:04 -0300

    BugFix: Do not allow gunicorn to hang and added header for process

--- docker-start.sh
+++ docker-start.sh
@@ -7,10 +7,27 @@ cd /netapi/dbmigrate; db-migrate --show-sql
 mysql -u root -h netapi_db networkapi < /netapi/dev/load_example_environment.sql
 
 echo -e "PYTHONPATH=\"/netapi/networkapi:/netapi/$PYTHONPATH\"" >> /etc/environment
-echo -e '#!/bin/bash\n/usr/local/bin/gunicorn -c /netapi/gunicorn.conf.py networkapi_wsgi:application' > /etc/init.d/gunicorn_networkapi
+
+cat > /etc/init.d/gunicorn_networkapi <<- EOM
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          scriptname
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
+/usr/local/bin/gunicorn -c /netapi/gunicorn.conf.py networkapi_wsgi:application
+EOM
+
 chmod 777 /etc/init.d/gunicorn_networkapi
 update-rc.d gunicorn_networkapi defaults
-
 export PYTHONPATH="/netapi/networkapi:/netapi/$PYTHONPATH"
-# cd /netapi; python manage.py runserver 0.0.0.0:8000
-/usr/local/bin/gunicorn  -c /netapi/gunicorn.conf.py networkapi_wsgi:application
+
+echo "starting gunicorn"
+/usr/local/bin/gunicorn -c /netapi/gunicorn.conf.py networkapi_wsgi:application
+
+tail -f /tmp/gunicorn-networkapi_error.log
