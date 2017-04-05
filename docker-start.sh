527730aae9ffa8bd7c82858ba3d4627699ff7535
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-04-05 16:43:11 -0300

    Base shell that provision the Network API service

--- docker-start.sh
+++ docker-start.sh
@@ -1,4 +1,14 @@
 
+mysql -uroot -h netapi_db -e 'drop database if exists networkapi;'
+mysql -uroot -h netapi_db -e 'create database networkapi;'
+cd /netapi/dbmigrate; db-migrate --show-sql
+mysql -u root -h netapi_db networkapi < /netapi/dev/load_example_environment.sql
 
 
-python manage.py runserver 0.0.0.0:8000
+echo -e "PYTHONPATH=\"/netapi/networkapi:/netapi/$PYTHONPATH\"" >> /etc/environment
+echo -e '#!/bin/bash\n/usr/local/bin/gunicorn -c /netapi/gunicorn.conf.py networkapi_wsgi:application' > /etc/init.d/gunicorn_networkapi
+chmod 777 /etc/init.d/gunicorn_networkapi
+update-rc.d gunicorn_networkapi defaults
+
+export PYTHONPATH="/netapi/networkapi:/netapi/$PYTHONPATH"
+/usr/local/bin/gunicorn  -c /netapi/gunicorn.conf.py networkapi_wsgi:application
