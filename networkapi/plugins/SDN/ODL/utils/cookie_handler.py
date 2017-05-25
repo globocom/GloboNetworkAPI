82882f41a1f2bc701ec7df67ba0d16b69b253034
juanaugusto <juan.augusto@ufrj.br>
2017-05-25 11:52:56 -0300

    ODL Cookie Handler: Changing variable names

--- networkapi/plugins/SDN/ODL/utils/cookie_handler.py
+++ networkapi/plugins/SDN/ODL/utils/cookie_handler.py
@@ -10,12 +10,12 @@ class CookieHandler(object):
     """
 
     @staticmethod
-    def get_cookie(id_acl, src_port=0, dest_port=0):
+    def get_cookie(id_acl, src_port=0, dst_port=0):
         id_acl = format(int(id_acl), '032b')
         src_port = format(int(src_port), '016b')
-        dest_port = format(int(dest_port), '016b')
+        dst_port = format(int(dst_port), '016b')
 
-        cookie = id_acl + src_port + dest_port
+        cookie = id_acl + src_port + dst_port
 
         return int(cookie, 2)
 
@@ -32,7 +32,7 @@ class CookieHandler(object):
         return int(cookie[32:48], 2)
 
     @staticmethod
-    def get_dest_port(cookie):
+    def get_dst_port(cookie):
         cookie = format(cookie, '064b')
 
         return int(cookie[48:64], 2)
