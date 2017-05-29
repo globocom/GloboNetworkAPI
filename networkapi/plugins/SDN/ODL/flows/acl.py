02b20a17d6c28b6fd2c8eb8e47bbf9dea3695f89
juanaugusto <juan.augusto@ufrj.br>
2017-05-29 08:52:39 -0300

    AclFlowBuilder: Renaming class method

--- networkapi/plugins/SDN/ODL/flows/acl.py
+++ networkapi/plugins/SDN/ODL/flows/acl.py
@@ -184,7 +184,7 @@ class AclFlowBuilder(object):
             self._build_description(rule)
             self._build_id(rule)
 
-            self._check_if_is_last_iteration(port, port_end)
+            self._insert_new_flow(port, port_end)
 
     def _build_specific_src_and_dst_ports_in_range(self, rule):
 
@@ -210,12 +210,12 @@ class AclFlowBuilder(object):
                 self._build_description(rule)
                 self._build_id(rule)
 
-                self._check_if_is_last_iteration(dst_port, dst_port_end)
+                self._insert_new_flow(dst_port, dst_port_end)
 
-            self._check_if_is_last_iteration(src_port, src_port_end)
+            self._insert_new_flow(src_port, src_port_end)
 
 
-    def _check_if_is_last_iteration(self, port, port_end):
+    def _insert_new_flow(self, port, port_end):
 
         if port < port_end:
             self.flows["flow"].insert(0, deepcopy(self.flows["flow"][0]))
