7b59258e1e3a23a7cc096e334a49175a5fbb0269
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-06-06 14:30:12 -0300

    Test runner script using docker: Add remote controller environment variable

--- docker-start-test.sh
+++ docker-start-test.sh
@@ -2,5 +2,5 @@ export NETWORKAPI_DEBUG=DEBUG
 export CI=1
 export NETWORKAPI_DATABASE_OPTIONS='{"init_command": "SET default_storage_engine=INNODB"}'
 export DJANGO_SETTINGS_MODULE='networkapi.settings_ci'
-export REMOTE_CTRL_IP=netapi_odl
+export REMOTE_CTRL_IP=$(nslookup netapi_odl | grep Address | tail -1 | awk '{print $2}')
 python manage.py jenkins "$@"
