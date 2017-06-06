5fa741cdd3903428300e2a4c0146565ab39a474d
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-06-06 11:11:49 -0300

    Docker start script: Add verbosity to docker start script

--- docker-start-opendaylight.sh
+++ docker-start-opendaylight.sh
@@ -8,16 +8,19 @@
 SLEEP_TIME=20
 
 # Enter the project directory
-cd /odl/distribution-karaf-0.5.0-Boron
+cd /opt/opendaylight/
 
 # Runs ODL in background using Karaf
+echo "Starting OpenDaylight.."
 ./bin/start
 
 # Waits until ODL is up and running
 sleep ${SLEEP_TIME}
 
 # Attaches to Karaf client and install some features for ODL
+echo "Installing features.."
 ./bin/client feature:install odl-restconf odl-l2switch-switch odl-dlux-all odl-netconf-connector-all odl-openflowplugin-flow-services
 
 # Attaches to Karaf server as a foreground process
+echo "Attaching on server.."
 ./bin/karaf server
