6647f390e4bfba3ddda55e4f8f9c4eaa31a0ce95
juanaugusto <juan.augusto@ufrj.br>
2017-06-12 17:10:19 -0300

    CookieHandler Tests: Fix tests

--- networkapi/plugins/SDN/ODL/tests/test_cookie_handler.py
+++ networkapi/plugins/SDN/ODL/tests/test_cookie_handler.py
@@ -11,35 +11,35 @@ class CookieFieldHandlerTestCaseSuccess(NetworkApiTestCase):
         """Test if get_cookie method returns right value for
             ACL id 150000 and operation type 2."""
 
-        assert_equal(CookieHandler.get_cookie(150000, 2),
-                     644245631270912)
+        assert_equal(CookieHandler.get_cookie(150000),
+                     644245094400000)
 
     def test_get_cookie_returns_ok_2(self):
         """Test if get_cookie method returns right value for
             ACL id 85434 and operation type 1."""
 
-        assert_equal(CookieHandler.get_cookie(85434, 1),
-                     366936504401920)
+        assert_equal(CookieHandler.get_cookie(85434),
+                     366936235966464)
 
     def test_get_cookie_returns_ok_3(self):
         """Test if get_cookie method returns right value for
             ACL id 76535 and operation type 15."""
 
-        assert_equal(CookieHandler.get_cookie(76535, 15),
-                     328719348531200)
+        assert_equal(CookieHandler.get_cookie(76535),
+                     328715321999360)
 
     def test_get_cookie_returns_ok_4(self):
         """Test if get_cookie method returns right value for
             ACL id 25194623 and operation type 11."""
 
-        assert_equal(CookieHandler.get_cookie(25194623, 11),
-                     108210084772839424)
+        assert_equal(CookieHandler.get_cookie(25194623),
+                     108210081820049408)
 
     def test_get_id_acl_returns_ok_1(self):
         """Test if get_id_acl method returns right value for
             cookie 644245631270912."""
 
-        cookie = 644245631270912
+        cookie = 644245094400000
         assert_equal(CookieHandler.get_id_acl(cookie),
                      150000)
 
@@ -47,7 +47,7 @@ class CookieFieldHandlerTestCaseSuccess(NetworkApiTestCase):
         """Test if get_id_acl method returns right value for
             cookie 366936504401920."""
 
-        cookie = 366936504401920
+        cookie = 366936235966464
         assert_equal(CookieHandler.get_id_acl(cookie),
                      85434)
 
@@ -55,7 +55,7 @@ class CookieFieldHandlerTestCaseSuccess(NetworkApiTestCase):
         """Test if get_id_acl method returns right value for
             cookie 328719348531200."""
 
-        cookie = 328719348531200
+        cookie = 328715321999360
         assert_equal(CookieHandler.get_id_acl(cookie),
                      76535)
 
@@ -63,38 +63,7 @@ class CookieFieldHandlerTestCaseSuccess(NetworkApiTestCase):
         """Test if get_id_acl method returns right value for
             cookie 108210084772839424."""
 
-        cookie = 108210084772839424
+        cookie = 108210081820049408
         assert_equal(CookieHandler.get_id_acl(cookie),
                      25194623)
 
-    def test_get_op_type_returns_ok_1(self):
-        """Test if get_op_type method returns right value for
-            cookie 644245631270912."""
-
-        cookie = 644245631270912
-        assert_equal(CookieHandler.get_op_type(cookie),
-                     2)
-
-    def test_get_op_type_returns_ok_2(self):
-        """Test if get_op_type method returns right value for
-            cookie 366936504401920."""
-
-        cookie = 366936504401920
-        assert_equal(CookieHandler.get_op_type(cookie),
-                     1)
-
-    def test_get_op_type_returns_ok_3(self):
-        """Test if get_op_type method returns right value for
-            cookie 328719348531200."""
-
-        cookie = 328719348531200
-        assert_equal(CookieHandler.get_op_type(cookie),
-                     15)
-
-    def test_get_op_type_returns_ok_4(self):
-        """Test if get_op_type method returns right value for
-            cookie 108210084772839424."""
-
-        cookie = 108210084772839424
-        assert_equal(CookieHandler.get_op_type(cookie),
-                     11)
