0502bc1881406cd34b6fa3e27e3fbe1d869df734
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-06-28 15:47:07 -0300

    ACL flow tests: Create tests for add, delete and update flows

--- networkapi/api_environment/tests/test_acl_flows.py
+++ networkapi/api_environment/tests/test_acl_flows.py
@@ -53,11 +53,50 @@ class APIEnvironmentFlowsTestCase(NetworkApiTestCase):
 
         response = self.client.put(
             self.FLOW_URL % (self.ENV_ID, data['rules'][0]['id']),
-            data,
-            HTTP_AUTHORIZATION=self.get_http_authorization('test'))
+            dumps(data),
+            HTTP_AUTHORIZATION=self.get_http_authorization('test'),
+            content_type="application/json")
 
-    def test_delete_acl_flow(self):
-        pass
+        resp_data = loads(response.content)
 
-    def test_update_acl_flow(self):
-        pass
+        assert response.status_code == 200
+        assert resp_data['id'] == str(data['rules'][0]['id'])
+
+    @patch("networkapi.plugins.SDN.ODL.Generic.ODLPlugin._flow")
+    def test_delete_acl_flow(self, *args):
+        """ Should delete an ACL flow """
+        plugin = args[0]
+
+        self.client.delete(
+            self.FLOW_URL % (self.ENV_ID, 1),
+            HTTP_AUTHORIZATION=self.get_http_authorization('test'),
+            content_type="application/json")
+
+        assert plugin.called is True
+        call = plugin.call_args
+        assert call[1]['method'] == "delete"
+        assert call[1]['flow_id'] == '1'
+
+    def test_update_acl_flow(self, *args):
+        """ Should update an ACL flow using async call """
+
+        data = {
+            "kind": "default#acl",
+            "rules": [{
+                "id": 1,
+                "protocol": "udp",
+                "description": "simple flow",
+                "source": "10.0.0.1/32",
+                "destination": "10.0.0.2/32",
+            }]
+        }
+
+        response = self.client.put(
+            self.FLOW_URL % (self.ENV_ID, data['rules'][0]['id']),
+            dumps(data),
+            HTTP_AUTHORIZATION=self.get_http_authorization('test'),
+            content_type="application/json")
+
+        assert response.status_code == 200
+        resp_data = loads(response.content)
+        assert resp_data['id'] == str(data['rules'][0]['id'])
