daeba2e10a79bf1460143bc083377717e6b570d9
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-08-04 17:42:23 -0300

    Refactoring: Clean up methods and Pep8

--- networkapi/plugins/SDN/ODL/Generic.py
+++ networkapi/plugins/SDN/ODL/Generic.py
@@ -118,13 +118,13 @@ class ODLPlugin(BaseSdnPlugin):
             raise exceptions.ValueInvalid()
 
         nodes_ids = self._get_nodes_ids()
-        if len(nodes_ids)<1:
+        if len(nodes_ids) < 1:
             raise exceptions.ControllerInventoryIsEmpty(msg="No nodes found")
 
         return_flows = []
         for node_id in nodes_ids:
-            path = "/restconf/config/opendaylight-inventory:nodes/node/%s/flow-node-inventory:table/0/flow/%s" \
-                   % (node_id, flow_id)
+            path = "/restconf/config/opendaylight-inventory:nodes/node/%s/" \
+                   "flow-node-inventory:table/0/flow/%s" % (node_id, flow_id)
 
             return_flows.append(
                 self._request(
@@ -138,49 +138,64 @@ class ODLPlugin(BaseSdnPlugin):
         """
         :return: All flows for table 0
         """
+
         nodes_ids = self._get_nodes_ids()
-        if len(nodes_ids)<1:
+        if len(nodes_ids) < 1:
             raise exceptions.ControllerInventoryIsEmpty(msg="No nodes found")
 
-        flows_list_by_switch = {}
+        flows_list = {}
         for node_id in nodes_ids:
             try:
-                path = "/restconf/config/opendaylight-inventory:nodes/node/%s/flow-node-inventory:table/0/"\
-                       % (node_id)
+                path = "/restconf/config/opendaylight-inventory:nodes/node/" \
+                       "%s/flow-node-inventory:table/0/" % (node_id)
 
-                flows_list_by_switch[node_id] = self._request(method="get", path=path, contentType='json')["flow-node-inventory:table"]
+                inventory = self._request(
+                    method="get",
+                    path=path,
+                    contentType='json'
+                )
+
+                flows_list[node_id] = inventory["flow-node-inventory:table"]
 
             except HTTPError as e:
                 if e.response.status_code == 404:
-                    flows_list_by_switch[node_id] =[]
+                    flows_list[node_id] = []
                 else:
-                    raise exceptions.CommandErrorException(msh=self._parse_errors(e.response.json()))
+                    raise exceptions.CommandErrorException(
+                        msh=self._parse_errors(e.response.json()))
             except Exception as e:
                 raise e
 
+        return flows_list
 
-        return flows_list_by_switch
     def _get_nodes_ids(self):
-        """
-        Returns a list of nodes ids controlled by ODL
-        """
+        """ Returns a list of nodes ids controlled by ODL """
+
         nodes = self._get_nodes()
+
         nodes_ids = []
         for node in nodes:
             nodes_ids.append(node["id"])
+
         return nodes_ids
 
     def _get_nodes(self):
+        """ Request the list of all controlled nodes from the environment """
+
         path = "/restconf/config/opendaylight-inventory:nodes/"
         nodes = self._request(method='get', path=path, contentType='json')
-        retorno = []
-        if nodes['nodes'].has_key('node'):
+
+        nodes_list = []
+        if 'node' in nodes['nodes']:
             for node in nodes['nodes']['node']:
                 if node["id"] not in ["controller-config"]:
-                    retorno.append(node)
-        return retorno
+                    nodes_list.append(node)
+
+        return nodes_list
 
     def _request(self, **kwargs):
+        """ Sends request to controller """
+
         # Params and default values
         params = {
             'method': 'get',
@@ -221,27 +236,20 @@ class ODLPlugin(BaseSdnPlugin):
 
             try:
                 return json.loads(request.text)
-            except:
+            except Exception as exception:
+                log.error("Can't serialize as Json: %s" % exception)
                 return
 
         except AttributeError:
             log.error('Request method must be valid HTTP request. '
                       'ie: GET, POST, PUT, DELETE')
-        # except HTTPError as e:
-        #
-        #     try:
-        #         response = json.loads(request.text)
-        #         for error in response["errors"]["error"]:
-        #             log.error(error["error-message"])
-        #     except:
-        #         log.error("Unknown error during request to ODL Controller")
-        #
-        #     raise HTTPError("Error during request to ODL Controller. Code %s" % request.status_code)
 
     def _get_auth(self):
         return self._basic_auth()
 
     def _basic_auth(self):
+        """ Create a HTTP Basic Authentication object """
+
         return HTTPBasicAuth(
             self.equipment_access.user,
             self.equipment_access.password
@@ -251,6 +259,7 @@ class ODLPlugin(BaseSdnPlugin):
         pass
 
     def _get_headers(self, contentType):
+        """ Creates HTTP headers needed by the plugin """
         types = {
             'json': 'application/yang.data+json',
             'xml':  'application/xml',
@@ -261,8 +270,9 @@ class ODLPlugin(BaseSdnPlugin):
                 'Accept': types[contentType]}
 
     def _get_equipment_access(self):
-        try:
+        """ Tries to get the equipment access """
 
+        try:
             access = None
             try:
                 access = EquipamentoAcesso.search(
