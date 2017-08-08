a5422750492bae9a530d348fbd8b84c72f820358
laurapanzariello <laura.panzariello@corp.globo.com>
2017-08-08 11:04:45 -0300

    datacenter post tests

---
+++ networkapi/api_rack/tests/fabric/test_fabric.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import os
+
+from django.test.client import Client
+
+from networkapi.test import load_json
+from networkapi.test.test_case import NetworkApiTestCase
+
+
+class RackTestCase(NetworkApiTestCase):
+    pass
