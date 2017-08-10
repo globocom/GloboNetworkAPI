6c17f43afd826a69981d430866d25c905faf7949
juanaugusto <juan.augusto@ufrj.br>
2017-08-10 09:18:12 -0300

    Refactor DELL BGP plugin

--- networkapi/plugins/Dell/FTOS/BGP/Generic.py
+++ networkapi/plugins/Dell/FTOS/BGP/Generic.py
@@ -2,6 +2,21 @@ from xml.etree.ElementTree import Element
 from xml.etree.ElementTree import SubElement
 from xml.etree.ElementTree import tostring
 from collections import OrderedDict
+from networkapi.equipamento.models import Equipamento
+from networkapi.equipamento.models import EquipamentoAcesso
+from networkapi.plugins.Dell.FTOS.BGP.exceptions import InvalidNeighborException
+from networkapi.plugins import exceptions
+import logging
+import requests
+from requests.auth import HTTPBasicAuth
+from requests.exceptions import HTTPError
+from schema import Schema
+from schema import SchemaError
+from schema import And
+from schema import Use
+from schema import Optional
+
+log = logging.getLogger(__name__)
 
 map_fields = {
     'remote_ip': 'neighbor-router',
@@ -25,45 +40,88 @@ map_fields = {
 
 class Generic(object):
 
-    def treat_neighbor(self, neighbor):
-        """Change Neighbor input """
+    def __init__(self, equipment=None, neighbor=None):
 
-        if neighbor['soft_reconfiguration'] == 0:
-            del neighbor['soft_reconfiguration']
-        else:
-            neighbor['soft_reconfiguration'] = 'inbound'
+        self.equipment = equipment
+        self.neighbor = neighbor
+        self.equipment_access = None
 
-        if neighbor['community'] == 0:
-            del neighbor['community']
-        else:
-            neighbor['community'] = ''
+    def _process_neighbor(self):
+        """Validate and Change Neighbor input because some tags can't might be
+           present when these tags are False.
+        """
 
-        return neighbor
+        self._validate_neighbor()
+        self._treat_soft_reconfiguration()
+        self._treat_community()
 
-    def order_neighbor(self, neighbor):
+    def _validate_neighbor(self):
+        """Validate neighbor against Schema."""
+
+        neighbor_schema = Schema({
+            'remote_ip': str,
+            Optional('remote_as'): lambda n: 0 <= int(n) <= 4294967295,
+            Optional('password'): str,
+            Optional('maximum_hops'): lambda n: 1 <= int(n) <= 255,
+            Optional('timer_keepalive'): lambda n: 1 <= int(n) <= 65535,
+            Optional('timer_timeout'): lambda n: 3 <= int(n) <= 65536,
+            Optional('description'): str,
+            Optional('soft_reconfiguration'): bool,
+            Optional('community'): bool,
+            Optional('remove_private_as'): bool,
+            Optional('next_hop_self'): bool
+        })
+
+        try:
+            neighbor_schema.validate(self.neighbor)
+
+        except SchemaError as e:
+            raise InvalidNeighborException(e.code)
+
+    def _treat_soft_reconfiguration(self):
+
+        if self.neighbor.get('soft_reconfiguration') is True:
+            self.neighbor['soft_reconfiguration'] = 'inbound'
+        elif self.neighbor.get('soft_reconfiguration') is False:
+            del self.neighbor['soft_reconfiguration']
+
+        # If soft_reconfiguration is not present, anything happens
+
+    def _treat_community(self):
+
+        if self.neighbor.get('community') is True:
+            self.neighbor['community'] = ''
+        elif self.neighbor.get('community') is False:
+            del self.neighbor['community']
+
+        # If community is not present, anything happens
+
+    def _order_neighbor(self):
         """Reorder fields in neighbor dict transforming it later
            in an OrderedDict.
         """
 
-        remote_ip = neighbor['remote_ip']
-        del neighbor['remote_ip']
+        # TODO test tomorrow
 
-        remote_as = neighbor['remote_as']
-        del neighbor['remote_as']
+        remote_ip = self.neighbor['remote_ip']
+        del self.neighbor['remote_ip']
 
-        neighbor = OrderedDict(neighbor)
+        remote_as = self.neighbor['remote_as']
+        del self.neighbor['remote_as']
+
+        self.neighbor = OrderedDict(self.neighbor)
 
         # When generate XML, remote_ip should be the first, and remote_as
         # the second.
-        neighbor.update({'remote_as': remote_as})
-        neighbor.update({'remote_ip': remote_ip})
+        self.neighbor.update({'remote_as': remote_as})
+        self.neighbor.update({'remote_ip': remote_ip})
 
-        return neighbor
+        return self.neighbor
 
-    def json_to_xml(self, neighbor):
+    def dict_to_xml(self):
 
-        neighbor = self.treat_neighbor(neighbor)
-        neighbor = self.order_neighbor(neighbor)
+        self._process_neighbor()
+        self._order_neighbor()
 
         bgp_xml = Element('bgp')
 
@@ -73,12 +131,12 @@ class Generic(object):
 
         # Walk the OrderedDict neighbor in Reversed Order to generate XML with
         # remote_ip in first position and remote_as in second position.
-        for field in reversed(neighbor):
+        for field in reversed(self.neighbor):
 
             if isinstance(map_fields[field], basestring):
 
                 child_xml = SubElement(neighbor_xml, map_fields[field])
-                child_xml.text = self._to_str(neighbor[field])
+                child_xml.text = self._to_str(self.neighbor[field])
 
             elif isinstance(map_fields[field], dict):
 
@@ -89,29 +147,148 @@ class Generic(object):
 
                     child_of_child_xml = SubElement(child_xml,
                                                     map_fields[field][child])
-                    child_of_child_xml.text = self._to_str(neighbor[field])
+                    child_of_child_xml.text = self._to_str(self.neighbor[field])
 
         return tostring(bgp_xml)
 
-    def _create_or_get_element(self, root, tag):
+    def deploy_neighbor(self):
+
+        path = "api/running/dell/router/bgp/"
+        data = self.dict_to_xml()
+        self._request(method='patch', path=path, data=data, content_type='xml')
+
+    def undeploy_neighbor(self):
+
+        pass
+
+    def _request(self, **kwargs):
+
+        # Params and default values
+        params = {
+            'method': None,
+            'path': None,
+            'data': None,
+            'content_type': None,
+        }
+
+        # Setting params via kwargs or use the defaults
+        for param in params:
+            if param in kwargs:
+                params[param] = kwargs.get(param)
+
+        headers = self._get_headers(content_type=params['content_type'])
+        uri = self._get_uri(path=params['path'])
+
+        log.info(
+            'Starting %s request to equipment %s at %s. Data to be sent: %s' %
+            (params['method'], self.equipment.nome, uri, params['data'])
+            )
+
+        try:
+            # Raises AttributeError if method is not valid
+            func = getattr(requests, params['method'])
+            request = func(
+                uri,
+                auth=self._get_auth(),
+                headers=headers,
+                data=params['data']
+            )
+
+            request.raise_for_status()
+
+            try:
+                return request.text
+            except:
+                return
+
+        except AttributeError:
+            log.error('Request method must be valid HTTP request. '
+                      'ie: GET, POST, PUT, DELETE')
+        except HTTPError:
+            try:
+                response = request.text
+
+            except:
+                log.error("Unknown error during request to Dell Equipment.")
+
+            raise HTTPError(request.status_code)
+
+    def _get_auth(self):
+        return self._basic_auth()
+
+    def _basic_auth(self):
+        return HTTPBasicAuth(
+            self.equipment_access.user,
+            self.equipment_access.password
+        )
+
+    def _o_auth(self):
+        pass
+
+    def _get_headers(self, content_type):
+        types = {
+            'xml':  'application/vnd.yang.data+xml',
+            'text': 'text/plain'
+        }
+
+        return {'Content-Type': types[content_type]}
+
+    def _get_equipment_access(self):
+        try:
+            return EquipamentoAcesso.search(
+                None, self.equipment, 'http').uniqueResult()
+        except Exception:
+            log.error('Access type %s not found for equipment %s.' %
+                      ('http', self.equipment.nome))
+            raise exceptions.InvalidEquipmentAccessException()
+        # TODO: ver o metodo existente, bater com o host (http com http)
+
+    def _get_host(self):
+
+        if not hasattr(self, 'host') or self.host is None:
+
+            if not isinstance(self.equipment_access, EquipamentoAcesso):
+
+                log.error('No fqdn could be found for equipment %s .' %
+                          (self.equipment.nome))
+                raise exceptions.InvalidEquipmentAccessException()
+
+            self.host = self.equipment_access.fqdn.strip()
+            if self.host.find('://') < 0:
+                self.host = self.protocol + '://' + self.host
+
+        return self.host
+
+    def _get_uri(self, host=None, path=""):
+
+        if host is None:
+            host = self._get_host()
+
+        host = host.strip()
+        path = path.strip()
+
+        if host[len(host) - 1] == '/':
+            host = host[0:len(host) - 1]
+        if path[0] == '/':
+            path = path[1:len(path)]
+        self.uri = host + '/' + path
+
+        return self.uri
+
+
+    @staticmethod
+    def _create_or_get_element(root, tag):
 
         if root.find(tag) is not None:
             return root.find(tag)
         else:
             return SubElement(root, tag)
 
-    def _to_str(self, value):
+    @staticmethod
+    def _to_str(value):
 
         if isinstance(value, bool):
             return str(value).lower()
         else:
             return str(value)
-
-    def deploy_neighbor(self):
-
-        pass
-
-    def undeploy_neighbor(self):
-
-        pass
 
