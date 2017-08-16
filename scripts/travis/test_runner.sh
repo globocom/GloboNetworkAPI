d4fa223e9b35597cd9474220c86b80a30ed18205
henriquebonadio <henrique.bonadio@corp.globo.com>
2017-08-16 19:49:04 -0300

    Travis CI: separate wait for container logic into another script

--- scripts/travis/test_runner.sh
+++ scripts/travis/test_runner.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-echo "Waiting for ODL to go up"
-
-MAX_RETRY=18
-SLEEP_TIME=10
-
-for i in $(seq 1  ${MAX_RETRY}); do
-
-    docker exec ovs1 ovs-vsctl show | grep is_connected > /dev/null
-
-    # If the port is open we continue with the script
-    if [ "$?" -eq "0" ]; then
-        break;
-    fi
-
-    # When maximum retries is achieved we exit with an error message
-    if [ "$i" -eq "${MAX_RETRY}" ]; then
-        echo "Max retry achieved"
-        exit 1;
-    fi
-
-    echo "Retrying ${i}.."
-    sleep ${SLEEP_TIME}
-done
-
-echo "ODL server is up"
-
 echo "exporting NETWORKAPI_DEBUG"
 export NETWORKAPI_DEBUG='DEBUG'
 
