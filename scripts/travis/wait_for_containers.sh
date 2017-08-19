570fac16c9da011d555420213730453dbd8518a0
henriquebonadio <henriquebonadio@hotmail.com>
2017-08-19 16:43:47 -0300

    Travis Ci: test diferent ovs start

--- scripts/travis/wait_for_containers.sh
+++ scripts/travis/wait_for_containers.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# NETAPI_ODL
+# test
+cid=$(docker run -itd --network globonetworkapi_netapi_net --name ovs4 --cap-add NET_ADMIN --cap-add SYS_MODULE -v /lib/modules:/lib/modules socketplane/openvswitch)
+docker exec $cid modprobe openvswitch
+docker exec $cid supervisorctl restart ovs-vswitchd
 
+
+# NETAPI_ODL
 echo "Waiting for ODL to go up"
 
 MAX_RETRY=30
@@ -10,7 +15,7 @@ SLEEP_TIME=10
 for i in $(seq 1  ${MAX_RETRY}); do
     
     sleep ${SLEEP_TIME}
-    docker exec ovs1 ovs-vsctl show | grep is_connected > /dev/null
+    docker exec ovs4 ovs-vsctl show | grep is_connected > /dev/null
 
     # If the port is open we continue with the script
     if [ "$?" -eq "0" ]; then
