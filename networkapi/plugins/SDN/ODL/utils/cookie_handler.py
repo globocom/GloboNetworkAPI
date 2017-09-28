d480a21c25d1e0e29e417ec5a3e0961b17725a9e
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-09-28 19:05:14 -0300

    Openflow cookie: Refactor the cookie handler to use environment ID

--- networkapi/plugins/SDN/ODL/utils/cookie_handler.py
+++ networkapi/plugins/SDN/ODL/utils/cookie_handler.py
@@ -6,34 +6,26 @@ class CookieHandler(object):
         OpenFlow Specification and present in OpenVSwitch.
 
         Cookie field has 64 bits. The first 32-bits are assigned to the id
-        of ACL input. The next 4 bits are assigned to the operation type and
-        the remaining 28 bits are filled by zeros.
+        of ACL input. The next 32 bits are assigned to the ID of the
+        environment.
     """
 
-    @staticmethod
-    def get_cookie(id_acl, src_port=0, dst_port=0):
+    def __init__(self, id_acl, id_env):
+
         id_acl = format(int(id_acl), '032b')
-        src_port = format(int(src_port), '016b')
-        dst_port = format(int(dst_port), '016b')
+        id_environment = format(int(id_env), '032b')
 
-        cookie = id_acl + src_port + dst_port
+        self.cookie = id_acl + id_environment
 
-        return int(cookie, 2)
+    @property
+    def cookie(self):
+        return int(self.cookie, 2)
 
-    @staticmethod
-    def get_id_acl(cookie):
-        cookie = format(cookie, '064b')
+    def raw(self):
+        return self.cookie
 
-        return int(cookie[0:32], 2)
+    def get_id_acl(self):
+        return int(self.cookie[0:32], 2)
 
-    @staticmethod
-    def get_src_port(cookie):
-        cookie = format(cookie, '064b')
-
-        return int(cookie[32:48], 2)
-
-    @staticmethod
-    def get_dst_port(cookie):
-        cookie = format(cookie, '064b')
-
-        return int(cookie[48:64], 2)
+    def get_id_environment(self):
+        return int(self.cookie[32:64], 2)
