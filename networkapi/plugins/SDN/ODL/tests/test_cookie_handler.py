312d42c4606b975dc10b61805d85d35f935e8876
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-09-29 16:29:24 -0300

    Update tests of openflow cookie

--- networkapi/plugins/SDN/ODL/tests/test_cookie_handler.py
+++ networkapi/plugins/SDN/ODL/tests/test_cookie_handler.py
@@ -10,52 +10,41 @@ class CookieFieldHandlerTestCaseSuccess(NetworkApiTestCase):
     def test_get_cookie_returns_ok_1(self):
         """Test get_cookie for ACL id 150000."""
 
-        assert_equal(CookieHandler.get_cookie(150000),
-                     644245094400000)
+        assert_equal(CookieHandler(150000).cookie, 644245094400000)
 
     def test_get_cookie_returns_ok_2(self):
         """Test get_cookie for ACL id 85434."""
 
-        assert_equal(CookieHandler.get_cookie(85434),
+        assert_equal(CookieHandler(85434).cookie,
                      366936235966464)
 
     def test_get_cookie_returns_ok_3(self):
         """Test get_cookie for ACL id 76535."""
 
-        assert_equal(CookieHandler.get_cookie(76535),
+        assert_equal(CookieHandler(76535).cookie,
                      328715321999360)
 
     def test_get_cookie_returns_ok_4(self):
         """Test get_cookie for ACL id 25194623."""
 
-        assert_equal(CookieHandler.get_cookie(25194623),
+        assert_equal(CookieHandler(25194623).cookie,
                      108210081820049408)
 
     def test_get_id_acl_returns_ok_1(self):
         """Test get_id_acl for cookie 644245631270912."""
 
-        cookie = 644245094400000
-        assert_equal(CookieHandler.get_id_acl(cookie),
+        assert_equal(CookieHandler(150000).get_id_acl(),
                      150000)
 
     def test_get_id_acl_returns_ok_2(self):
         """Test get_id_acl for cookie 366936504401920."""
 
-        cookie = 366936235966464
-        assert_equal(CookieHandler.get_id_acl(cookie),
+        assert_equal(CookieHandler(85434, id_env=42).get_id_acl(),
                      85434)
 
-    def test_get_id_acl_returns_ok_3(self):
-        """Test get_id_acl for cookie 328719348531200."""
+    def test_get_id_environment(self):
+        """Test get_id_environment from cookie """
 
-        cookie = 328715321999360
-        assert_equal(CookieHandler.get_id_acl(cookie),
-                     76535)
-
-    def test_get_id_acl_returns_ok_4(self):
-        """Test get_id_acl for cookie 108210084772839424."""
-
-        cookie = 108210081820049408
-        assert_equal(CookieHandler.get_id_acl(cookie),
-                     25194623)
+        assert_equal(CookieHandler(25194623, id_env=10).get_id_environment(),
+                     10)
 
