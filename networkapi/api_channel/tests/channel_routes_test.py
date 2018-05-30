f0f4dc81dc18b2d0f9b33d8a7613ea1e6c694e8a
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2018-05-30 14:55:08 -0300

    Create first tests to Channel API implementation

--- networkapi/api_channel/tests/channel_routes_test.py
+++ networkapi/api_channel/tests/channel_routes_test.py
@@ -15,13 +15,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from json import dumps
 
-from unittest import TestCase
+from django.test.client import Client
+
+from rest_framework.exceptions import AuthenticationFailed
+
+from networkapi.test.test_case import NetworkApiTestCase
+from networkapi.api_channel.views import DeployChannelConfV3View
 
 
-class TestChannelRoutes(TestCase):
+class TestChannelRoutes(NetworkApiTestCase):
     """ Class to test Channels http routes """
 
-    def test_true(self):
-        """ Should be True """
-        self.assertTrue(True)
+    fixtures = [
+        'networkapi/system/fixtures/initial_variables.json',
+        'networkapi/usuario/fixtures/initial_usuario.json',
+        'networkapi/grupo/fixtures/initial_ugrupo.json',
+        'networkapi/usuario/fixtures/initial_usuariogrupo.json',
+        'networkapi/grupo/fixtures/initial_permissions.json',
+        'networkapi/grupo/fixtures/initial_permissoes_administrativas.json',
+
+#        "networkapi/api_channel/fixtures/initial_channel.json"
+    ]
+
+    def setUp(self):
+        self.client = Client()
+        self.auth = self.get_http_authorization('test')
+
+    def test_should_receive_method_not_allowed(self):
+        """ Should receive method not allowed """
+
+        response = self.client.get(
+            '/api/v3/interface/channel/1/deploy/',
+            content_type='application/json',
+            HTTP_AUTHORIZATION=self.auth)
+
+        self.assertEqual(405, response.status_code)
+
+    def test_should_not_allow_unauthenticated_user(self):
+        """ Should not allow requests from an unauthenticated user """
+
+        with self.assertRaises(AuthenticationFailed):
+
+            response = self.client.put(
+                '/api/v3/interface/channel/1/deploy/',
+                data=dumps({}),
+                content_type='application/json',
+                HTTP_AUTHORIZATION=self.get_http_authorization('fake'))
