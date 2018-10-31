276782719dfb0ad4c34d91f16f0a0e95b1fbb6a0
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2018-10-31 17:05:51 -0300

    Add asynchronous flow deletion using OpenDaylight plugin

--- networkapi/api_environment/tasks/flows.py
+++ networkapi/api_environment/tasks/flows.py
@@ -18,4 +18,14 @@ def async_flush_environment(self, plugins, user_id, data):
     """ Asynchronous flush and restore of flows of an environment """
 
     for plugin in plugins:
-        plugin.update_all_flows(data=data)
+        if plugin is not None:
+            plugin.update_all_flows(data=data)
+
+
+@celery_app.task(bind=True, base=BaseTask, serializer='pickle')
+def async_delete_flow(self, plugins, user_id, flow_id):
+    """ Asynchronous delete one flow by id """
+
+    for plugin in plugins:
+        if plugin is not None:
+            plugin.del_flow(flow_id=flow_id)
