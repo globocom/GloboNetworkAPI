50bb6e3ec206ce882eb9db15edfa8067432f49db
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2018-11-03 14:35:46 -0300

    Use Supervisord instead of SystemV

--- scripts/docker/docker-start-netapi.sh
+++ scripts/docker/docker-start-netapi.sh
@@ -59,46 +59,16 @@ cd /netapi/dbmigrate; db-migrate --show-sql
 
 
 echo "Loading base Network API data into database"
-mysql -u root -h netapi_db networkapi < /netapi/dev/load_example_environment.sql
+mysql -u root -h ${NETWORKAPI_DATABASE_HOST} networkapi < /netapi/dev/load_example_environment.sql
 
 
-# Updates the SDN controller ip address
-echo "Configuring ODL container's host address if exists"
-REMOTE_CTRL=$(nslookup netapi_odl | grep Address | tail -1 | awk '{print $2}')
+# Discovering SDN controller
+REMOTE_CTRL=$(nslookup ${NETWORKAPI_SDN_CTRL} | grep Address | tail -1 | awk '{print $2}')
 echo "$REMOTE_CTRL  odl.controller" >> /etc/hosts
+echo "Found SDN controller: ${REMOTE_CTRL}"
 
 
-# Adds project root directory to Python path
-echo -e "PYTHONPATH=\"/netapi/networkapi:/netapi/$PYTHONPATH\"" >> /etc/environment
-export PYTHONPATH="/netapi/networkapi:/netapi/$PYTHONPATH"
 
-
-# Creates System V init script
-# TODO: We should move to System D or Supervisor D
-cat > /etc/init.d/gunicorn_networkapi <<- EOM
-#!/bin/bash
-### BEGIN INIT INFO
-# Provides:          scriptname
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start daemon at boot time
-# Description:       Enable service provided by daemon.
-### END INIT INFO
-
-/usr/local/bin/gunicorn --pid $PIDFILE -c /netapi/gunicorn.conf.py wsgi:application
-EOM
-
-
-# Enable the init script
-chmod 777 /etc/init.d/gunicorn_networkapi
-update-rc.d gunicorn_networkapi defaults
-
-
-echo "Starting gunicorn"
-cd /netapi
-touch /tmp/gunicorn-networkapi_error.log
-/etc/init.d/gunicorn_networkapi
-
-tail -f /tmp/gunicorn-networkapi_error.log
+echo "Starting gunicorn using supervisord"
+/venv/bin/supervisord -c /netapi/scripts/docker/netapi_supervisord.conf
+/venv/bin/supervisorctl tail -f netapi stdout
