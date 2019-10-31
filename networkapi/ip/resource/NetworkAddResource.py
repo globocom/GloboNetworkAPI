8349b5bccb8f4054f9803341b434898e20a8031e
carlos.moura <carlos.moura@corp.globo.com>
2019-10-31 19:14:28 -0300

    fix NetworkEnvironmentError exception

--- networkapi/ip/resource/NetworkAddResource.py
+++ networkapi/ip/resource/NetworkAddResource.py
@@ -209,8 +209,13 @@ class NetworkAddResource(RestResource):
                                 return self.response_error(414)
 
                         else:
+<<<<<<< HEAD
                             raise NetworkEnvironmentError(None, 'O ambiente não está configurado. '
                                                                 'É necessário efetuar a configuração.')
+=======
+                            raise NetworkEnvironmentError(None, 'O ambiente não consta com rede cadastrada. '
+                                                                'É necessário cadastrar uma rede ao ambiente.')
+>>>>>>> fix NetworkEnvironmentError exception
 
                     except NetworkEnvironmentError:
                         self.log.error('The environment does not have a registered network')
