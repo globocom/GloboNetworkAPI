4e621c4282367d1677be02df488fed1e4f824c38
Leopoldo <leopoldo@yaveh.local>
2019-11-08 20:58:23 -0300

    Fix: syntaxwq

--- networkapi/plugins/SDN/ODL/Generic.py
+++ networkapi/plugins/SDN/ODL/Generic.py
@@ -91,6 +91,7 @@ class ODLPlugin(BaseSdnPlugin):
         except Exception as err:
             message = self._parse_errors(err.response.json())
             log.error("ERROR while removing a flow due to It does not exist anymore: %s" % message)
+
             pass
 
 
