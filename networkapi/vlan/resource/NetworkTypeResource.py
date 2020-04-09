68c08414bc30cbe712815bb0cacae10597a4cf37
carlos.moura <tre.junior@hotmail.com>
2020-04-09 18:34:54 -0300

    adding vlan name special character verification and fix PEP8 from files

--- networkapi/vlan/resource/NetworkTypeResource.py
+++ networkapi/vlan/resource/NetworkTypeResource.py
@@ -30,6 +30,7 @@ from networkapi.rest import RestResource
 from networkapi.util import is_valid_int_greater_zero_param
 from networkapi.util import is_valid_string_maxsize
 from networkapi.util import is_valid_string_minsize
+from networkapi.util import is_valid_vlan_name
 from networkapi.vlan.models import NetTypeUsedByNetworkError
 from networkapi.vlan.models import NetworkTypeNameDuplicatedError
 from networkapi.vlan.models import NetworkTypeNotFoundError
@@ -107,6 +108,11 @@ class NetworkTypeResource(RestResource):
                 raise InvalidValueError(None, 'name', name)
 
             net_type = TipoRede(tipo_rede=name)
+
+            if not is_valid_vlan_name(name):
+                self.log.error(
+                    u'Parameter %s is invalid because is using special characters and/or breaklines.', name)
+                raise InvalidValueError(None, 'name', name)
 
             try:
                 TipoRede.get_by_name(net_type.tipo_rede)
--- networkapi/vlan/resource/VlanAllocateResource.py
+++ networkapi/vlan/resource/VlanAllocateResource.py
@@ -36,6 +36,7 @@ from networkapi.rest import RestResource
 from networkapi.util import is_valid_int_greater_zero_param
 from networkapi.util import is_valid_string_maxsize
 from networkapi.util import is_valid_string_minsize
+from networkapi.util import is_valid_vlan_name
 from networkapi.vlan.models import Vlan
 from networkapi.vlan.models import VlanError
 from networkapi.vlan.models import VlanNameDuplicatedError
@@ -90,6 +91,11 @@ class VlanAllocateResource(RestResource):
             # Name must NOT be none and 50 is the maxsize
             if not is_valid_string_minsize(name, 3) or not is_valid_string_maxsize(name, 50):
                 self.log.error(u'Parameter name is invalid. Value: %s.', name)
+                raise InvalidValueError(None, 'name', name)
+
+            if not is_valid_vlan_name(name):
+                self.log.error(
+                    u'Parameter %s is invalid because is using special characters and/or breaklines.', name)
                 raise InvalidValueError(None, 'name', name)
 
             # Description can NOT be greater than 200
