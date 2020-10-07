c38e72190a8c4fa379e3f38a590aac966d777b07
malinoski <iuri.malinoski@g.globo>
2020-10-07 13:55:06 -0300

    Mock tests was upgraded to assert the calls on the plugin functions

--- networkapi/plugins/Juniper/JUNOS/tests.py
+++ networkapi/plugins/Juniper/JUNOS/tests.py
@@ -7,34 +7,36 @@ docker exec -it netapi_app ./fast_start_test_reusedb.sh networkapi/plugins/Junip
 from networkapi.test.test_case import NetworkApiTestCase
 from networkapi.plugins.base import BasePlugin
 from networkapi.plugins.Juniper.JUNOS.plugin import JUNOS
-from mock import patch
+from mock import patch, MagicMock
 
 
 class JunosPluginTest(NetworkApiTestCase):
 
     def test_create_junos_plugin_class(self):
-        """
-        JunosPluginTest - test_create_junos_plugin_class - Tests if the class will be properly created
-        """
         plugin = JUNOS()
         self.assertIsInstance(plugin, BasePlugin)
 
-    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS.connect')
-    def test_call_connect(self, mock_plugin_function):
-        mock_plugin_function.assert_called()
+    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS', autospec=True)
+    def test_connect(self, mock_junos_plugin):
+        mock_junos_plugin.connect()
+        mock_junos_plugin.connect.assert_called_with()
 
-    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS.close')
-    def test_call_close(self, mock_plugin_function):
-        mock_plugin_function.assert_called()
+    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS', autospec=True)
+    def test_call_close(self, mock_junos_plugin):
+        mock_junos_plugin.close()
+        mock_junos_plugin.close.assert_called_with()
 
-    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS.exec_command')
-    def test_call_exec_command(self, mock_plugin_function):
-        mock_plugin_function.assert_called()
+    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS', autospec=True)
+    def test_call_exec_command(self, mock_junos_plugin):
+        mock_junos_plugin.exec_command("any command")
+        mock_junos_plugin.exec_command.assert_called_with("any command")
 
-    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS.copyScriptFileToConfig')
-    def test_call_copyScriptFileToConfig(self, mock_plugin_function):
-        mock_plugin_function.assert_called()
+    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS', autospec=True)
+    def test_call_copyScriptFileToConfig(self, mock_junos_plugin):
+        mock_junos_plugin.copyScriptFileToConfig("any file path")
+        mock_junos_plugin.copyScriptFileToConfig.assert_called_with("any file path")
 
-    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS.ensure_privilege_level')
-    def test_call_ensure_privilege_level(self, mock_plugin_function):
-        mock_plugin_function.assert_called()
+    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS', autospec=True)
+    def test_call_ensure_privilege_level(self, mock_junos_plugin):
+        mock_junos_plugin.ensure_privilege_level()
+        mock_junos_plugin.ensure_privilege_level.assert_called_with()
