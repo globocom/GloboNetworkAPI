e44c730e459a38ee9990e86166a10d1bdbaf3bdc
malinoski <iuri.malinoski@g.globo>
2020-10-09 10:16:25 -0300

    Mock tests was improved to reuse a mock class (equipment) and mock success inside target function (connect and exec_command)

--- networkapi/plugins/Juniper/JUNOS/tests.py
+++ networkapi/plugins/Juniper/JUNOS/tests.py
@@ -1,9 +1,3 @@
-"""
-How to use onlu this test:
-cd GloboNetworkAPI
-docker exec -it netapi_app ./fast_start_test_reusedb.sh networkapi/plugins/Juniper/JUNOS
-
-"""
 from networkapi.test.test_case import NetworkApiTestCase
 from networkapi.plugins.base import BasePlugin
 from networkapi.plugins.Juniper.JUNOS.plugin import JUNOS
@@ -12,24 +6,87 @@ from mock import patch, MagicMock
 
 class JunosPluginTest(NetworkApiTestCase):
 
-    def test_create_junos_plugin_class(self):
+    """
+    Junos plugin tests
+
+    How to use:
+        cd GloboNetworkAPI
+        docker exec -it netapi_app ./fast_start_test_reusedb.sh networkapi/plugins/Juniper/JUNOS
+
+    General notes:
+        - autospec=True: responds to methods that actually exist in the real class
+        - assert_called_once_with() assert if an important step was called
+    """
+
+    @patch('networkapi.equipamento.models.EquipamentoAcesso', autospec=True)
+    def setUp(self, mock_equipment_access):
+        
+        """
+        Equipment mock is a fake class used in specific test cases below
+        """
+
+        mock_equipment_access.fqdn = 'any fqdn'
+        mock_equipment_access.user = 'any user'
+        mock_equipment_access.password = 'any password'
+        self.mock_equipment_access = mock_equipment_access
+
+    def test_create_junos_with_super_class(self):
         plugin = JUNOS()
         self.assertIsInstance(plugin, BasePlugin)
 
-    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS', autospec=True)
-    def test_connect(self, mock_junos_plugin):
-        mock_junos_plugin.connect()
-        mock_junos_plugin.connect.assert_called_with()
+    @patch('networkapi.plugins.Juniper.JUNOS.plugin.Device', autospec=True)
+    def test_connect_success(self, mock_device):
+
+        """
+        Simulate connect success using Junos Device mock.
+
+        Note: All internal functions in Device Class are mocked, raising no exceptions on it
+
+        Warning: For an unknown reason was not possible to use
+        'jnpr.junos.Device' instead 'networkapi.plugins.Juniper.JUNOS.plugin.Device'
+        """
+
+        # Mocking result
+        mock_device.return_value.connected = True
+
+        # Close real test
+        plugin = JUNOS(equipment_access=self.mock_equipment_access)
+        connection_response = plugin.connect()
+
+        # Asserts
+        plugin.remote_conn.open.assert_called_with()
+        self.assertIsNotNone(plugin.configuration)
+        self.assertEqual(connection_response, True)
+
+    @patch('jnpr.junos.utils.config.Config', autospec=True)
+    def test_exec_command_success(self, mock_config):
+
+        """
+        This test asserts the success of the complete workflow of executing any command.
+        Note: All internal functions in Config Class are mocked, raising no exceptions on it
+        """
+
+        # Mocking result
+        plugin = JUNOS(equipment_access=self.mock_equipment_access)
+        plugin.plugin_try_lock = MagicMock()
+        plugin.plugin_try_lock.return_value = True
+        plugin.configuration = mock_config
+
+        # Close real test
+        exec_command_response = plugin.exec_command("any command")
+
+        # Assert
+        plugin.configuration.rollback.assert_called_once_with()
+        plugin.configuration.load.assert_called_once_with("any command", format='set')
+        plugin.configuration.commit_check.assert_called_once_with()
+        plugin.configuration.commit.assert_called_once_with()
+        plugin.configuration.unlock.assert_called_once_with()
+        self.assertIsNotNone(exec_command_response)
 
     @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS', autospec=True)
     def test_call_close(self, mock_junos_plugin):
         mock_junos_plugin.close()
         mock_junos_plugin.close.assert_called_with()
-
-    @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS', autospec=True)
-    def test_call_exec_command(self, mock_junos_plugin):
-        mock_junos_plugin.exec_command("any command")
-        mock_junos_plugin.exec_command.assert_called_with("any command")
 
     @patch('networkapi.plugins.Juniper.JUNOS.plugin.JUNOS', autospec=True)
     def test_call_copyScriptFileToConfig(self, mock_junos_plugin):
